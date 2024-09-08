e9bcaeffb94e33d4c570149d1523ccfe814fcd0e
Mahamudu-Halic <mahamuduhalic129@gmail.com>
2024-09-08 02:28:55 +0000

    types added

---
+++ src/types/propTypes.ts
@@ -0,0 +1,15 @@
+export interface InputFieldProps {
+  label: string;
+  type?: string;
+}
+
+export interface FormFieldProps {
+  title: String;
+  buttonLabel: String;
+  createAccount?: Boolean;
+}
+
+export interface ButtonProps {
+  title: String;
+  variant?: String;
+}
